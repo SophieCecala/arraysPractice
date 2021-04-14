b130476185c288329dd21b27a1ad8dfbc30e1ee3
Natalya Myunster <natalyamyunster@yahoo.com>
2021-04-14 16:42:40 +0200

    task 1 task 2

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const arrLenght = prompt("write a number");
+    /*task 1 */
+function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    const arrLenght = prompt("write a number");
+
+    const arr = [];
+
+    for(let i = 0; i < arrLenght; i++){
+        arr[i] = getRandomArbitrary(-100, 100);
+    }
+
+//document.write(arr);
+
+        /*task 2*/
+for(let i = 0; i < arr.length; i++){
+    if(i % 3 === 2){
+       document.write(`  <strong> ${arr[i]} </strong> `);
+    }else {
+        document.write(` ${arr[i]} `);
+    }
+
+}
+
