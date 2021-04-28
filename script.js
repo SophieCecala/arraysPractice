1ceec645416ad23ea2d5a88b4f96479e447de68a
Natalya Myunster <natalyamyunster@yahoo.com>
2021-04-28 16:43:06 +0200

    task 1 task 2 task 3 task 4

--- script.js
+++ script.js
@@ -14,20 +14,23 @@ function getRandomArbitrary(min, max) {
 //document.write(arr);
 
         /*task 2, 3, 4 */
-    let max = -Infinity;
 
- for(let i = 0; i < arr.length; i++) {
+    console.log('MAX ', Math.max(...arr));
+    const max = Math.max(...arr);
 
-     if (arr[i] % 3 === 0) {
-         document.write `<font style="color: red"> ${arr[i]} </font>`
-     } else if (arr[i] % 3 === 2) {
-         document.write(`  <strong> ${arr[i]} </strong> `);
-     } else {
-         document.write(` ${arr[i]} `);
+    for(let i = 0; i < arr.length; i++) {
+      if (i % 3 === 2 && arr[i] === max) {
+          document.write(`<font style="color: green"><strong>${arr[i]}</strong></font> `);
+      }else if(arr[i] === max ){
+            document.write(`<font style="color: green">${arr[i]}</font> `);
+      }else if (i % 3 === 2 && arr[i] % 3 === 0) {
+          document.write(`<font style="color: red"><strong>${arr[i]}</strong></font> `);
+      }else if( i % 3 === 2){
+         document.write(`<strong>${arr[i]}</strong> `);
+     }else if(arr[i] % 3 === 0){
+         document.write(`<font style="color: red">${arr[i]}</font> `)
+     } else{
+         document.write(`${arr[i]} `);
      }
-     if(arr[i] > max){
-         max = arr[i];
-     }else{
-         document.write(`<font style="color: green"> ${max} </font>`)
-     }
- }
+    }
+
