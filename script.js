78fbaa630848bc1a7e0cc70288b0edabfd0774e5
Natalya Myunster <natalyamyunster@yahoo.com>
2021-05-12 16:41:47 +0200

    task 1 task 2 task 3 task 4 task 5 task 6 and task 7 ( not finished )

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
     /*task 1 */
-function getRandomArbitrary(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
+// function getRandomArbitrary(min, max) {
+//         return Math.floor(Math.random() * (max - min)) + min;
+//     }
+//
+//     const arrLenght = prompt("write a number");
+//
+//     const arr = [];
+//
+//     for(let i = 0; i < arrLenght; i++){
+//         arr[i] = getRandomArbitrary(-100, 100);
+//     }
+//
+//
+//     console.log('MAX ', Math.max(...arr));
+//     const max = Math.max(...arr);
+//
+//
+//     let num = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         num = arr[i];
+//         if (i % 3 === 2){
+//             num =  `<strong>${num}</strong> `
+//         }
+//         if (arr[i] === max){
+//             num = `<font style="color: green">${num}</font> `
+//         }
+//         if (i % 3 === 0){
+//             num = `<font style="color: red">${num}</font> `
+//         }
+//         if (arr[i] < 0){
+//             num = `<ins> ${num}</ins> `
+//         }
+//         if (arr[i] < 10 && arr[i] > -10){
+//             num = `<i> ${num}</i> `
+//         }
+//         document.write(num)
+//     }
+
+    const fruits = ["banana", "watermelon", "strawberry", "lemon", "ananas", "kiwi", "mango"];
+
+    let maxLength = 0;
+
+    for(let i = 0; i < fruits.length; i++) {
+        if(fruits[i].length > maxLength ){
+            maxLenght = fruits[i].length;
+        }
+
     }
-
-    const arrLenght = prompt("write a number");
-
-    const arr = [];
-
-    for(let i = 0; i < arrLenght; i++){
-        arr[i] = getRandomArbitrary(-100, 100);
-    }
-
-//document.write(arr);
-
-        /*task 2, 3, 4 */
-
-    console.log('MAX ', Math.max(...arr));
-    const max = Math.max(...arr);
-
-    for(let i = 0; i < arr.length; i++) {
-      if (i % 3 === 2 && arr[i] === max) {
-          document.write(`<font style="color: green"><strong>${arr[i]}</strong></font> `);
-      }else if(arr[i] === max ){
-            document.write(`<font style="color: green">${arr[i]}</font> `);
-      }else if (i % 3 === 2 && arr[i] % 3 === 0) {
-          document.write(`<font style="color: red"><strong>${arr[i]}</strong></font> `);
-      }else if( i % 3 === 2){
-         document.write(`<strong>${arr[i]}</strong> `);
-     }else if(arr[i] % 3 === 0){
-         document.write(`<font style="color: red">${arr[i]}</font> `)
-     }else if(arr[i] < 0 && arr[i] % 3 === 0 && i % 3 === 2){
-          document.write(`<font style="color: red"><strong><ins>${arr[i]}</ins></strong></font> `)
-      }else if(arr[i] < 0 && arr[i] === max && i % 3 === 2){
-          document.write(`<font style="color: green"><strong><ins>${arr[i]}</ins></strong></font> `)
-      }else if(arr[i] < 0 && arr[i] % 3 === 0){
-          document.write(`<font style="color: red"<ins>${arr[i]}</ins></font> `)
-      }else if(arr[i] < 0 && i % 3 === 2){
-          document.write(`<ins><strong>${arr[i]}</strong></ins> `)
-      }else if(arr[i] < 0){
-          document.write(`<ins>${arr[i]}</ins>` )
-      }else{
-         document.write(`${arr[i]} `);
-     }
-    }
-
-    for(let i = 0; i < arr.lenght; i++){
-        if (i % 3 ===2)
-    }
-
+    document.write(maxLength);
